6dcfe95f730a06748bd265e9a86722a48de9f5a9
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-12-05 02:07:37 +0500

    name and slogan added

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import logo from "./assets/logo.png"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Drive Shine</Text>
-      <StatusBar style="auto" />
+      <View style={styles.textConatiner}>
+        <Text style={styles.text}>Drive Shine</Text>
+        <Text style={styles.sloganText}>Your Car's Spa on Wheels</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#d37a7a",
+  },
+  textConatiner: {
+    paddingTop: 100,
+    paddingLeft: 30,
+  },
+  text: {
+    backgroundColor: "#ffc800",
+    fontWeight: "bold",
+    fontSize: 40,
+  },
+  sloganText: {
+    backgroundColor: "#968e6c",
+    fontSize: 20,
+    paddingLeft: 20,
   },
 });
